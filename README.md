4691a0a8e0ff711e727affd4dca36ef60c8d851f
dcho02 <chung@cayenne.co.jp>
2024-01-07 23:43:52 +0900

    add: MUI

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 node version: 20.5.1
 
-
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>hello</h1>
+      <Button variant="contained">Hello world</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
